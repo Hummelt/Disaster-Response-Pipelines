94a4784dbe1582eb6765b0bf3b94936106a7749d
M. Hummelt <71187881+Hummelt@users.noreply.github.com>
2021-05-03 15:03:08 +0200

    Create run.py

---
+++ app/run.py
@@ -0,0 +1,167 @@
+import json
+import plotly
+import pandas as pd
+
+import nltk
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+import re
+
+from flask import Flask
+from flask import render_template, request, jsonify
+from plotly.graph_objs import Bar
+import joblib
+#from sklearn.external.joblib import joblib
+from sqlalchemy import create_engine
+
+
+app = Flask(__name__)
+
+# ****************** Functions to generate data for the visualizations *****************************
+
+
+def tokenize(text):
+    '''Function that takes in a text splits with white spaces and creates list of words'''
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower())
+
+    word_list = nltk.tokenize.word_tokenize(text)
+
+    stop = stopwords.words("english")
+    word_list = [t for t in word_list if t not in stop]
+
+    lemmed_list = [WordNetLemmatizer().lemmatize(w) for w in word_list]
+
+    return lemmed_list
+
+
+def word_bagger(w_lst):
+    '''Function that iterates through a list of texts, bundles separate words with count, sort by count and stored top 15 in a dataframe'''
+    word_dict = {}
+    for message in w_lst:
+        lemm_lst = tokenize(message)
+        for w in lemm_lst:
+            if w in word_dict:
+                word_dict[w] += 1
+            else:
+                word_dict.update({w: 1})
+
+    word_df = pd.DataFrame.from_dict(word_dict, orient='index', columns=[
+                                     'Count']).sort_values('Count', ascending=False).head(15)
+    return word_df
+
+
+# load data
+engine = create_engine('sqlite:///../data/DisasterResponse.db')
+df = pd.read_sql_table('disaster_messages', engine)
+
+# load model
+model = joblib.load("../models/classifier.pkl")
+
+
+# index webpage displays cool visuals and receives user input text for model
+@app.route('/')
+@app.route('/index')
+def index():
+
+    # extract data needed for visuals
+    # TODO: Below is an example - modify to extract data for your own visuals
+    genre_counts = df.groupby('genre').count()['message']
+    genre_names = list(genre_counts.index)
+
+    # Data for visualization 2
+    cat_counts = df.iloc[:, 4:].sum().sort_values(ascending=False)
+    cat_names = df.iloc[:, 4:].columns
+
+    # Data for visualisation 3
+    word_counts = word_bagger(df['message'])['Count']
+    word_names = word_bagger(df['message']).index.tolist()
+
+    # create visuals
+    # TODO: Below is an example - modify to create your own visuals
+    graphs = [
+        {
+            'data': [
+                Bar(
+                    x=genre_names,
+                    y=genre_counts
+                )
+            ],
+
+            'layout': {
+                'title': 'Distribution of Message Genres',
+                'yaxis': {
+                    'title': "Count"
+                },
+                'xaxis': {
+                    'title': "Genre"
+                }
+            }
+        },
+
+        {
+            'data': [
+                Bar(
+                    x=cat_names,
+                    y=cat_counts
+                )
+            ],
+            'layout': {
+                'title': 'Disaster Category Distribution',
+                'yaxis': {
+                    'title': "Count of classifications"
+                }
+            }
+        },
+
+        {
+            'data': [
+                Bar(
+                    x=word_names,
+                    y=word_counts,
+                )
+            ],
+            'layout': {
+                'title': 'Top 15 used words',
+                'yaxis': {
+                    'title': "Count of appearances"
+                },
+                'xaxis': {
+                    'title': "Words"
+                }
+            }
+        }
+    ]
+
+    # encode plotly graphs in JSON
+    ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
+    graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
+
+    # render web page with plotly graphs
+    return render_template('master.html', ids=ids, graphJSON=graphJSON)
+
+
+# web page that handles user query and displays model results
+@app.route('/go')
+def go():
+    # save user input in query
+    query = request.args.get('query', '')
+
+    # use model to predict classification for query
+    classification_labels = model.predict([query])[0]
+    classification_results = dict(zip(df.columns[4:], classification_labels))
+
+    # This will render the go.html Please see that file.
+    return render_template(
+        'go.html',
+        query=query,
+        classification_result=classification_results
+    )
+
+
+def main():
+    app.run(host='0.0.0.0', port=3001, debug=True)
+
+
+if __name__ == '__main__':
+    main()
